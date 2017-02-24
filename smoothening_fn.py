6cccf6150fe5635ac93f72c2592b0eab22f81b64
dcasasor <daniel.casas@udea.edu.co>
2017-02-24 18:59:12 -0500

    Just finishing...

--- smoothening_fn.py
+++ smoothening_fn.py
@@ -9,54 +9,59 @@ from __future__ import division
 import numpy as np
 import scipy.misc as misc
 import matplotlib.pyplot as plt
-from matplotlib import rcParams
 
-# Define a function to compute etha: smoothing function for replacing step function
-def etha(t, ts, n, tau):
-    n_sum = np.arange(n)
-    etha_time = np.zeros(len(t))
-    for ind, time in enumerate(t):
-        sum_ind        = 1/misc.factorial(n_sum) * (n * (time - t_s)/tau)**n_sum
-        sum_term       = sum(sum_ind)
-        etha_time[ind] = 1 - np.exp(- n * (time - t_s)/tau) * sum_term
-    
+
+def etha(time, time_s, n_param, tau):
+    """ Define a function to compute etha: smoothing function to replace
+    step function.
+    """
+    n_sum = np.arange(n_param)
+    etha_time = np.zeros(len(time))
+    for ind, time in enumerate(time):
+        sum_ind = 1/misc.factorial(n_sum) * (n_param * (time - t_s)/tau)**n_sum
+        sum_term = sum(sum_ind)
+        etha_time[ind] = 1 - np.exp(- n_param * (time - t_s)/tau) * sum_term
+
     return etha_time
-    
-# --------------------------------------------------------------------- Figures
-    
-rcParams['font.family'] = 'serif'
-rcParams['font.size'] = 14
-rcParams['text.size'] =14
-rcParams['text.usetex'] = True
 
-# ---------------------------------- Replicating Fig. 2
-plt.figure()
-t_span  = np.linspace(0, .05, 1000)
-t_s     = 0 # seconds (this is important, take the time of event as zero!!)
-n_reg   = 3
-tau_reg = 1e-2
-    
-for order in range(1, 5):
-    etha_fun = etha(t_span, t_s, order, tau_reg)
-    plt.plot(t_span, etha_fun, label='n = %s' % order)
-    
-plt.legend(loc=4)
-plt.text(.02, .04, r'$\tau = %s$' %tau_reg)
 
-# ---------------------------------- Replicating Fig. 1
-plt.figure()
+if __name__ == '__main__':
+    plt.style.use('~/Dropbox/phd_degree/general/python/daniel_latex.mplstyle')
 
-lines = ['-k', '--k', '-.k', ':k']
+    # ---------------------------------- Replicating Fig. 2
+    fig_order, axis_order = plt.subplots(figsize=(3.3, 2.06))
 
-for i, tau_it in enumerate( np.array([.001, .01, .05, .1]) ):
-    etha_fun_2 = etha(6*t_span, t_s, n_reg, tau_it)
-    plt.plot(6*t_span, etha_fun_2, lines[i], label='tau = %s' % tau_it, lw=1.5)
+    t_span = np.linspace(0, .05, 1000)
+    t_s = 0  # seconds (this is important, take the time of event as zero!!)
+    n_reg = 2
+    tau_reg = 1e-2
 
-plt.legend(loc='best', frameon=0)
-plt.xlim(xmax=0.3)
-plt.ylim(ymax=1.05)
-plt.xlabel('t')
-plt.ylabel(r'\eta')
-plt.text(0.25, 0.18, 'n = %s' % n_reg)
-plt.text(.15, .4, r'$\eta(t - t_s, n, \tau) = 1 - exp \left( -n \frac{(t - t_s)}{\tau} \right) \sum_{j = 0}^{n-1}{ \frac{1}{j!} \left( n \frac{(t-t_s)}{\tau} \right)}$',
-         bbox=dict(facecolor='w', alpha=1))
+    for order in range(1, 5):
+        etha_eval = etha(t_span, t_s, order, tau_reg)
+        axis_order.plot(t_span, etha_eval, label='$n = %s$' % order)
+
+    axis_order.text(.02, .04, r'$\tau = %s$' % tau_reg)
+    axis_order.set_xlim(xmax=0.05)
+    axis_order.legend(frameon=False)
+
+    plt.savefig('img/n_var.pdf', bbox_inches='tight')
+
+    # ---------------------------------- Replicating Fig. 1
+    fig_tau, axis_tau = plt.subplots(figsize=(3.3, 2.06))
+
+    lines = ['-k', '--k', '-.k', ':k']
+    tau_array = np.array([.001, .01, .05, .1])
+
+    for i, tau in enumerate(tau_array):
+        etha_fun_2 = etha(6*t_span, t_s, n_reg, tau)
+        axis_tau.plot(6*t_span, etha_fun_2, lines[i],
+                      label=r'$\tau = %s$' % tau, lw=1.5)
+
+    axis_tau.legend(loc='best', frameon=0)
+    axis_tau.set_xlim(xmax=6*t_span[-1])
+    axis_tau.set_ylim()
+    axis_tau.set_xlabel('time')
+    axis_tau.set_ylabel('$\eta(t - t_s, n, $' r'$\tau)$')
+    axis_tau.text(0.24, 0.8, '$n = %s$' % n_reg)
+
+    plt.savefig('img/tau_var.pdf', bbox_inches='tight')
